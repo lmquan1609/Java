cb050ed2daa938815091c8e1a2455d6a8715a3b9
lmquan1609 <luuminhquan155@gmail.com>
2022-11-27 17:29:18 +0700

    First commit

---
+++ src/com/lmquan/BankAccount.java
@@ -0,0 +1,71 @@
+package com.lmquan;
+
+
+public class BankAccount {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+    public BankAccount() {
+        this("456465", 2.5, "Default name", "Default Address", "Default phone");
+        System.out.println("Empty constructor called");
+    }
+
+    public BankAccount(String number, double balance,
+                    String customerName, String customerEmailAddress,
+                    String customerPhoneNumber
+    ) {
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmailAddress = customerEmailAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public void deposit(double depositAmount) {
+        this.balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        if (this.balance < withdrawalAmount) {
+            System.out.println("Only " + balance + " available. Withdrawal not processed");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance is " + this.balance);
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+}
---
+++ src/com/lmquan/Main.java
@@ -0,0 +1,9 @@
+package com.lmquan;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount quanAccount = new BankAccount("12345", 0.00, "Quan Luu", "quanlm@gmail.com", "0909123");
+        quanAccount.withdrawal(100.0);
+        quanAccount.deposit(50.0);
+    }    
+}
